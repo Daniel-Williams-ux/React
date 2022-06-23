aa250f00e978fa41bafe4b5aeb695e22e712f1cd
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-23 15:40:27 +0000

    Why React? It's composable

--- index.jsx
+++ index.jsx
@@ -1,9 +1,30 @@
-//ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
+ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
 
-//ReactDOM.render(<p>Hi, am devdan</p>, document.querySelector('#root'))
+ReactDOM.render(<p>Hi, am devdan</p>, document.querySelector('#root'))
 
 /*Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 ReactDOM.render(
   <ul><li>first item</li><li>second item</li></ul>,
-document.getElementById("root"))
+document.getElementById("root")) 
+
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+// Afterward, render it below the Navbar (which
+// you can do inside the ReactDOM.render call below)
+
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
