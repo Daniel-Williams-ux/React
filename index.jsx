4e336643d7cd0097dee96d50c15635fcfb82cf70
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-23 16:44:43 +0000

    created an imperative code

--- index.jsx
+++ index.jsx
@@ -28,3 +28,19 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 )
+
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+const h1 = document.createElement("h1")
+const textContent = "This is an imperative way to program";
+const className = header;
+document.getElementById("root").append(h1)
