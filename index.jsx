22a49fd0494dd7b2d81a0f41fa3b2bc01eb3abb1
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-23 14:01:21 +0000

    using querySelector to get the same result

--- index.jsx
+++ index.jsx
@@ -1,1 +1,3 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
+//ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
+
+ReactDOM.render(<p>Hi, am devdan</p>, document.querySelector('#root'))
