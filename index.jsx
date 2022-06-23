b14c4c3a658a056cdbc412c002bdef7551ebce33
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-23 14:12:24 +0000

    first practice

--- index.jsx
+++ index.jsx
@@ -1,3 +1,9 @@
 //ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
 
-ReactDOM.render(<p>Hi, am devdan</p>, document.querySelector('#root'))
+//ReactDOM.render(<p>Hi, am devdan</p>, document.querySelector('#root'))
+
+/*Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+  <ul><li>first item</li><li>second item</li></ul>,
+document.getElementById("root"))
