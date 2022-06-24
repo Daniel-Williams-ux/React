7a911c4cdd763d7d8ca19dd2e233fbdcb60052d0
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-24 05:27:41 +0000

    append in action

--- index.jsx
+++ index.jsx
@@ -124,3 +124,34 @@ const navbar = (
 )
 
 ReactDOM.render(navbar, document.getElementById("root"))
+
+
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
+
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
+
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+document.getElementById("root").append(page) //=>[object Object]
+document.getElementById("root").append(JSON.strigify(page))//=> object view
+
+//This shows that JSX returns JS objects. It's the ReactDOM that interpretes it.
