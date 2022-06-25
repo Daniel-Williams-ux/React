fd4f8818c7e0fd96c6d5c6dbb1366e040f4d4c27
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 11:45:22 +0000

    Parent/Child component

--- index.jsx
+++ index.jsx
@@ -362,3 +362,43 @@ function Header() {
 }
 
 ReactDOM.render(<Header />, document.getElementById("root"))
+
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
