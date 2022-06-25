cbadf290676a94ed031d7fdcee79043ef4f24704
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 16:00:32 +0000

    import Footer from "./Footer"

--- index.jsx
+++ index.jsx
@@ -406,16 +406,7 @@ ReactDOM.render(<Page />, document.getElementById("root"))
 import React from "react"
 import ReactDOM from "react-dom"
 import Header from "./Header"
-
-/**
-Challenge: 
-
-- Move the `footer` into its own component called "Footer" 
-  and render that component inside the Page component.
-- Move the `h1` and `ol` together into another component
-  called "MainContent" and render inside Page as well.
-*/
-
+import Footer from "./Footer"
 
 function MainContent() {
     return (
