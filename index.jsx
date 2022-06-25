ddf08ad629b52621a59f8d89a17903318cd11517
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 10:55:29 +0000

    Challenge

--- index.jsx
+++ index.jsx
@@ -303,4 +303,62 @@ function Page() {
 }
 
 ReactDOM.render(<Page />, document.getElementById("root"))
-  
+
+/*
+  Quiz!
+
+1. What is a React component?
+
+
+2. What's wrong with this code?
+```
+function myComponent() {
+    return (
+        <small>I'm tiny text!</small>
+    )
+}
+```
+
+3. What's wrong with this code?
+```
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+ReactDOM.render(Header(), document.getElementById("root"))
+```
+*/
+
+//Quiz!
+
+//1. What is a React component?
+//A function that returns React elements. (UI)
+
+//2. What's wrong with this code?
+
+function MyComponent() {
+    return (
+        <small>I'm tiny text!</small>
+    )
+}
+
+
+//3. What's wrong with this code?
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+ReactDOM.render(<Header />, document.getElementById("root"))
