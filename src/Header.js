5412a9fbf732f808c2f88a4fce00ecadc9ca8762
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 15:45:35 +0000

    organize components

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./react-logo.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header // if this is the only component
