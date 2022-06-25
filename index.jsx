29296c9b67e869835830f6da5aa539515c319b5f
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 11:47:35 +0000

    added header component

--- index.jsx
+++ index.jsx
@@ -386,7 +386,7 @@ function Header() {
 function Page() {
     return (
         <div>
-            
+            <Header />
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
