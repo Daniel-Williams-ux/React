ee03f7c134d626d3bbc8870897ad99d1f39941e7
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 16:06:44 +0000

    import MainContent from "./MainContent"

--- index.jsx
+++ index.jsx
@@ -403,11 +403,17 @@ function Page() {
 
 ReactDOM.render(<Page />, document.getElementById("root"))
 
+
+
+
+  
+
 import React from "react"
 import ReactDOM from "react-dom"
 import Header from "./Header"
 import Footer from "./Footer"
-
+import MainContent from "./MainContent"
+  
 function Page() { //parent component
     return (
         <div>
