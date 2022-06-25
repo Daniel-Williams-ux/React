680a58ef97274f74b449e0ad0c7d7414e233aff8
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-25 15:58:35 +0000

    created a footer component

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
