90f8371a43bb3baf460fda67c6ab6921c3a1e0fb
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-17 12:15:40 +0100

    Update more_info.js

--- more_info.js
+++ more_info.js
@@ -1,3 +1,4 @@
+
 /*
 Static Web pages
 1. Read-only, no changes to data.
@@ -13,17 +14,67 @@ Examples:
 */
 
 /* Event Listener*/
+
+//index.html file
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="index.pack.js"></script>
+    </body>
+</html>
+
+//css
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    padding: 30px;
+    background-color: whitesmoke;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.container > img {
+    width: 100%;
+    border-radius: 10px;
+    transition: filter .1s ease-in-out;
+}
+
+.container > img:hover {
+    filter: brightness(50%);
+}
+
+.container > button {
+    width: 100%;
+    border-radius: 10px;
+    padding: 1rem;
+    border: 2px solid lightgray;
+    background-color: white;
+    cursor: pointer;
+    margin-top: 20px;
+    font-family: 'Karla', sans-serif;
+    font-size: 1.3rem;
+}
+
+//app.jsx file
 import React from "react"
 
 export default function App() {
     function handleClick() {
         console.log("I was clicked!")
     }
-    
-    /**
-     * Challenge: 
-     * Log something to the console when the mouse hovers over the image
-     */
     
     return (
         <div className="container">
