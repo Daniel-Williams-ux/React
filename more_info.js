39b22cacb9a7844fc52dbfe0bd22647ee6e8bbc7
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-17 08:39:46 +0100

    Create more_info.js

---
+++ more_info.js
@@ -0,0 +1,13 @@
+/*
+Static Web pages
+1. Read-only, no changes to data.
+Examples:
+   blogs, news sites, recipes etc
+  
+Dynamic Web Apps
+1. Read-write: ability to change data
+2. Highly interactive
+3. displays your data
+Examples:
+   bank websites, airbnb, social media websites, online games etc
+*/
