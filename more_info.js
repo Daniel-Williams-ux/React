824ea768dda4f69049574ab9cd07b7b3a3e152ff
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-17 12:10:02 +0100

    Update more_info.js

--- more_info.js
+++ more_info.js
@@ -11,3 +11,24 @@ Dynamic Web Apps
 Examples:
    bank websites, airbnb, social media websites, online games etc
 */
+
+/* Event Listener*/
+import React from "react"
+
+export default function App() {
+    function handleClick() {
+        console.log("I was clicked!")
+    }
+    
+    /**
+     * Challenge: 
+     * Log something to the console when the mouse hovers over the image
+     */
+    
+    return (
+        <div className="container">
+            <img src="https://picsum.photos/640/360" />
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    )
+}
