c1795ea0961ab5e106f34b7abded492ba4fdf206
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-18 14:06:38 +0100

    Update more_info.js

--- more_info.js
+++ more_info.js
@@ -94,3 +94,121 @@ export default function App() {
         </div>
     )
 }
+
+
+
+
+//part two
+//index file
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="index.pack.js"></script>
+    </body>
+</html>
+
+
+//css file
+* {
+    box-sizing: border-box;
+}
+
+body {
+    background-color: #70B85D;
+    color: white;
+    padding: 20px;
+    font-family: 'Karla', sans-serif;
+    font-size: 1.3rem;
+}
+
+button {
+    width: 100%;
+    background-color: transparent;
+    padding: 1rem;
+    color: white;
+    border: 2px solid white;
+    border-radius: 50px;
+    cursor: pointer;
+    font-family: 'Karla', sans-serif;
+    margin-bottom: 20px;
+}
+
+button:hover {
+    background-color: #FFFEF1;
+    color: #2C5E2E;
+}
+
+button:focus {
+    outline: 0;
+}
+
+
+//app
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    const thingsArray = ["Thing 1", "Thing 2"]
+    
+    /**
+     * Challenge: Map over the thingsArray to generate
+     * a <p> element for each item and render them on the page
+     * below the button
+     */
+    
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+//the key gets rid of the warning sign
+    
+    return (
+        <div>
+            <button>Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Challenge: Add an event listener to the button so when
+ * it is clicked, it adds another thing to our array
+ * 
+ * Hint: use the array length + 1 to determine the number
+ * of the "Thing" being added. Also, have you event listener
+ * console.log(thingsArray) after adding the new item to the
+ * array
+ * 
+ * Spoiler: the page won't update when new things get added
+ * to the array!
+ */
+
+function App() {
+    const thingsArray = ["Thing 1", "Thing 2"]
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+    
+    function addItem() {
+        const newThingText = `Thing ${thingsArray.length + 1}`
+        thingsArray.push(newThingText)
+        console.log(thingsArray)
+    }
+    
+    return (
+        <div>
+            <button onClick={addItem}>Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
