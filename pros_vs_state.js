74d5f5a4e23164affe457e2a21ec4f8d9a71e6bf
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-23 06:59:44 +0100

    Update and rename pros_vs_state to pros_vs_state.js

---
+++ pros_vs_state.js
@@ -0,0 +1,68 @@
+/*
+"Props" refer to the properties being passed into a component in order for it to work correctly, similarly to how a function receives parameters:"from above"
+A component receiving props is not allowed to modify those props.(i.e. they are "immutable.")
+*/
+
+function addTwoNumbers(a, b) {
+    a = 42 //In the body we can't just modify parameters even though when you run it it gives you 44 as against 3.
+    return a + b //this is how a function receives parameters "from above".
+}
+
+addTwoNumbers(1, 2)
+
+//Simillarl in react
+function Navbar(props) {
+    props.coverImage = "something else" // this is a red flag because it should be immutable
+}
+//Note that from within the body of your function you should never change your props.
+
+
+/*
+"State" refers to values that are defined within the component, similar to variables defined inside a function.
+Anytime you have changing values that should be saved/displayed, you will likely be using state.
+*/
+
+function addTwoNumbers(a, b) {
+    a = 42  // Don't do this!
+    return a + b
+}
+
+addTwoNumbers(1, 2)
+
+/*
+Challenge: complete the function below
+Given a name, return "Good <timeOfDay>, <name>!" depending
+on the current time of day.
+
+4 am - 11:59 am, timeOfDay = "morning"
+12 pm - 4:59 pm: timeOfDay = "afternoon"
+5 pm - 7:59 pm: timeOfDay = "evening"
+8 pm - 3:59 am: timeOfDay = "night"
+
+E.g. if it's currently 1 pm, greeting("Jane") ==> "Good afternoon, Jane!"
+
+Hint: you can get the current time of day by using: 
+
+const date = new Date()
+const hours = date.getHours()
+*/
+
+function greeting(name) {
+    const date = new Date()
+    const hours = date.getHours()
+    
+    let timeOfDay
+    if(hours >= 4 && hours < 12) {
+        timeOfDay = "morning"
+    } else if(hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else if(hours >= 17 && hours < 20) {
+        timeOfDay = "evening"
+    } else {
+        timeOfDay = "night"
+    }
+    
+    return `Good ${timeOfDay}, ${name}!`
+}
+
+console.log(greeting("Bob"))
